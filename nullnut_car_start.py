c45cb5198bb7f5fa0b6ab2527d7872c559a7c15a
Brendan Weirtz <brendanweirtz@gmail.com>
2019-03-25 23:13:04 -0400

    Directory mix up, deleted nullnut-car and using nullnut_car

---
+++ nullnut_car_start.py
@@ -0,0 +1,5 @@
+import os
+
+
+os.system("echo '\n\n*----------------------------------------*\nNullnut Car System Status [| \e[42m RUNNING \e[0m |]\n*----------------------------------------*'\n\n")
+print("All systems are a go... Drive safe...\n\n")
